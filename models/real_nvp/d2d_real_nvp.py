c03365e40179d40b594d3739aa853c7877c15d7c
Andrew Kondrich <andrewk1@stanford.edu>
2018-12-12 02:03:11 -0800

    test if squeezsxing is problem

--- models/real_nvp/d2d_real_nvp.py
+++ models/real_nvp/d2d_real_nvp.py
@@ -35,15 +35,15 @@ class D2DRealNVP(RealNVP):
         #            Unsqueezing()]
         # Squeeze part
         for scale in range(num_scales):
-            layers += [Coupling(in_channels, mid_channels, num_blocks, MaskType.CHECKERBOARD, reverse_mask=False),
-                       Coupling(in_channels, mid_channels, num_blocks, MaskType.CHECKERBOARD, reverse_mask=True)]
+            # layers += [Coupling(in_channels, mid_channels, num_blocks, MaskType.CHECKERBOARD, reverse_mask=False),
+            #            Coupling(in_channels, mid_channels, num_blocks, MaskType.CHECKERBOARD, reverse_mask=True)]
 
-
+            in_channels *= 4  # Account for the squeeze
             mid_channels *= 2  # When squeezing, double the number of hidden-layer features in s and t
-            layers += [Squeezing(),
-                       Coupling(in_channels, mid_channels, num_blocks, MaskType.CHANNEL_WISE, reverse_mask=False),
-                       Coupling(in_channels, mid_channels, num_blocks, MaskType.CHANNEL_WISE, reverse_mask=True),
-                       Coupling(in_channels, mid_channels, num_blocks, MaskType.CHANNEL_WISE, reverse_mask=False)]
+            # layers += [Squeezing(),
+            #            Coupling(in_channels, mid_channels, num_blocks, MaskType.CHANNEL_WISE, reverse_mask=False),
+            #            Coupling(in_channels, mid_channels, num_blocks, MaskType.CHANNEL_WISE, reverse_mask=True),
+            #            Coupling(in_channels, mid_channels, num_blocks, MaskType.CHANNEL_WISE, reverse_mask=False)]
 
         for scale in range(num_scales):
             in_channels = int(in_channels / 4)  # Account for the unsqueeze
