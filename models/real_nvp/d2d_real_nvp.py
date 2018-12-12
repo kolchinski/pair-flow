Reversed MNIST and SVHN defaults, removed 2x factor from lambda_max, fixed bug in g (d2d), detached f from x2 backprop

models/real_nvp/d2d_real_nvp.py
@@ -99,7 +99,7 @@ class D2DRealNVP(RealNVP):
             assert(z_split is None)
 
             # Undo logits
-            x = torch.sigmoid(x)
+            x = torch.sigmoid(y)
 
             # Shape should stay constant - hourglass architecture image-to-image
             assert x.shape == x2.shape, f'x and x2 have different shapes: {x.shape}, {x2.shape}'