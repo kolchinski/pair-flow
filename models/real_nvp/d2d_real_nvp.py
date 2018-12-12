7456f57616aebc2afc73ea64bb5b99d70c300dad
Andrew Kondrich <andrewk1@stanford.edu>
2018-12-12 01:32:54 -0800

    Try modified arch for an overfit z to x2

--- models/real_nvp/d2d_real_nvp.py
+++ models/real_nvp/d2d_real_nvp.py
@@ -47,17 +47,10 @@ class D2DRealNVP(RealNVP):
 
         # Unsqueeze part
         for scale in range(num_scales):
-            layers += [Coupling(in_channels, mid_channels, num_blocks, MaskType.CHECKERBOARD, reverse_mask=False),
-                       Coupling(in_channels, mid_channels, num_blocks, MaskType.CHECKERBOARD, reverse_mask=True),
-                       Coupling(in_channels, mid_channels, num_blocks, MaskType.CHECKERBOARD, reverse_mask=False)]
-
             in_channels = int(in_channels / 4)  # Account for the unsqueeze
             mid_channels = int(mid_channels / 2)  # When unsqueezing, halve the number of hidden-layer features in
             # s and t
-            layers += [Unsqueezing(),
-                       Coupling(in_channels, mid_channels, num_blocks, MaskType.CHANNEL_WISE, reverse_mask=False),
-                       Coupling(in_channels, mid_channels, num_blocks, MaskType.CHANNEL_WISE, reverse_mask=True),
-                       Coupling(in_channels, mid_channels, num_blocks, MaskType.CHANNEL_WISE, reverse_mask=False)]
+            layers += [Unsqueezing()]
 
         self.layers = nn.ModuleList(layers)
 
