e00ca07caa4f7fcfe68477d5e3d7ddd75936f353
Alex Kolchinski <y.a.kolchinski@gmail.com>
2018-12-12 22:10:56 -0500

    Fixed optimizers for cuda

--- train.py
+++ train.py
@@ -128,8 +128,13 @@ def main(args):
 
     # TODO: We aren't doing the weight decay thing (see commented line above) - put this
     # back in if it seems like it was helpful
-    optimizer_rnvp = optim.Adam(net._modules['rnvp'].parameters(), lr=args.lr)
-    optimizer_d2d = optim.Adam(net._modules['d2d'].parameters(), lr=args.lr)
+
+    if device == 'cuda':
+        optimizer_rnvp = optim.Adam(net.module.rnvp.parameters(), lr=args.lr)
+        optimizer_d2d = optim.Adam(net.module.d2d.parameters(), lr=args.lr)
+    else:
+        optimizer_rnvp = optim.Adam(net.rnvp.parameters(), lr=args.lr)
+        optimizer_d2d = optim.Adam(net.d2d.parameters(), lr=args.lr)
     optimizers = {'rnvp' : optimizer_rnvp, 'd2d' : optimizer_d2d}
 
     for epoch in range(start_epoch, start_epoch + args.num_epochs):
